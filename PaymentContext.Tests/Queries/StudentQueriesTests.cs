bea39f35b32bdb5a73f77a5ba7abe61dd7fa2af6
adodriano <adodriano@gmail.com>
2024-04-20 05:36:25 -0300

    Creating queries and unitary tests

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,42 @@
+﻿using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using System.Linq.Expressions;
+
+namespace PaymentContext.Domain.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            _students = new List<Student>();
+            for (var i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("1111111111" + i.ToString(),
+                    EDocumentType.CPF),
+                    new Email("aluno" + i.ToString() + "@aluno.com")
+                    ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("12345678911");
+            var studnt = _students.AsQueryable().Where(exp).FirstOrDefault();
+            Assert.AreEqual(null, studnt);
+        }
+        
+        [TestMethod]
+        public void ShouldReturnStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("1111111111");
+            var studnt = _students.AsQueryable().Where(exp).FirstOrDefault();
+            Assert.AreEqual(null, studnt);
+        }
+    }
+}
