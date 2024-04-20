461c1050e8a8d650c622349c3c16dd78b507c71b
adodriano <adodriano@gmail.com>
2024-04-20 03:30:02 -0300

    create commands and unitary tests commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,24 @@
+﻿namespace PaymentContext.Domain.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+
+            var command = new CreateBoletoSubscriptionCommand();
+
+            command.FirstName = "";
+
+            command.Validate();
+
+            Assert.AreEqual(false, command.IsValid);
+
+
+        }
+
+
+
+    }
+}
